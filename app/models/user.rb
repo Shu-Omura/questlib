4ce9fd57a2950745b4cd27bf0d400f5481539cd2
Shu Oomura <ohs6261@gmail.com>
2020-09-04 14:30:00 +0900

    Add columns to users with name and tel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  validates :name, presence: true
+  validates :tel, format: { with: VALID_TEL_REGEX }, allow_nil: true, allow_blank: true
 end
